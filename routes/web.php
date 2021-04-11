45f6dcfbd1e5cc1a6a4e29e56dee3b963c758b43
Banu <bmemmedli88@gmail.com>
2021-04-12 01:38:08 +0400

    updated

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\AttributeController;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
@@ -15,5 +16,9 @@ Route::group(['prefix' => 'admin','as' => 'admin.'], function()
     //Categories
     Route::get('categories/status/{category}', [CategoryController::class, 'updatestatus'])->name('categories.status');
     Route::resource('categories', CategoryController::class);
+
+    //Attributes
+    Route::get('attributes/status/{attribute}', [AttributeController::class, 'updatestatus'])->name('attribute.status');
+    Route::resource('attributes', AttributeController::class);
 
 });
