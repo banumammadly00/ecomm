c215292d447c780d9685e91766e7325f7a8e6eb6
Banu <bmemmedli88@gmail.com>
2021-04-12 01:37:09 +0400

    atributes migrations added

---
+++ database/migrations/2021_04_10_111807_create_attributes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAttributesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attributes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->string('slug');
+            $table->string('values', 150)->default(0);
+            $table->string('attribute_type', 50);
+            $table->tinyInteger('status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attributes');
+    }
+}
