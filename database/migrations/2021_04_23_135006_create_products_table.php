bd1e239431a324f32a19baadfc3b6755e2a5d7f6
Banu <bmemmedli88@gmail.com>
2021-04-28 02:22:05 +0400

    products table

---
+++ database/migrations/2021_04_23_135006_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 150);
+            $table->bigInteger('sku_number');
+            $table->integer('amount');
+            $table->integer('discount_amount')->default(0);
+            $table->integer('count')->default(1);
+            $table->text('main_image');
+            $table->text('images');
+            $table->tinyInteger('status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
